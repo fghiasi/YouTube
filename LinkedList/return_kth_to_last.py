bb7f4b4936e60423ba14c64b80d7a1bb9c4969d2
Farbod Ghiasi <fghiasi@uci.edu>
2022-08-17 12:13:04 -0700

    Solving returning the kth to last element of a linked list using the runner method.

--- LinkedList/return_kth_to_last.py
+++ LinkedList/return_kth_to_last.py
@@ -1,82 +1,101 @@
-# 1- write dups from unsorted linked list
-# 2- what if temp buffer is Not allowd
+# From Cracking the Coding Interview Chapter 2 Linked List Question 2.2
+
+# Implement an algorithm to find the kth to last element of singly linked list.
+
+# Do we know the size of the Linked List? 
+# size - kth to last and we iterate from head to size - kth to last
 
 class Node:
-    def __init__(self, val=0, next=None):
-        self.val = val
+    def __init__(self, next=None, val=None):
         self.next = next
-    def print_linked_list(self, head):
-        print("Prinitng Linked List Values: ")
+        self.val = val
+
+# create a Linked List from list of entegers and returns a pointer to the head.
+class LinkedList:
+    
+    def __init__(self):
+        self.head = Node()
+
+        
+    def convert_list_to_linked_list(self, list_of_integers):
+        # if len(list_of_integers) == 0
+        if not list_of_integers:
+            return self.head
+        
+        
+        pointer_to_head = self.head
+        for num in list_of_integers:
+            temp_node = Node()
+            temp_node.val = num
+            
+            self.head.next = temp_node
+            self.head = self.head.next
+            
+        return pointer_to_head.next
+    
+    # prints the Linked list values.
+    def print_linked_list(self, head):    
         while head is not None:
-            print("Node val is = ", head.val)
+            print(head.val)
             head = head.next
 
-
-
-# 3- iterative 
-class solution:
-    # 1- Space = O(n)
-    # 2- time = O(n)
-    def remove_dups_with_buffer(self, ll_head):
-        if ll_head is None:
-            return ll_head
+class Solution:
+    # loop once and find the size
+    # return the node that is positioned at size - kth_to_last
+    # Running time O(n)
+    # Space O(1)
+#     def return_ll_kth_to_last(head, kth_to_last):
+#         # if kth_to_last == 0:
+#         #     return head
         
-        ll_head_holder = ll_head
-        seen = dict()
-        seen[ll_head.val] = True
+#         head_copy = head
+#         size = 0
+#         while head_copy is not None:
+#             size += 1
+#             head_copy = head_copy.next
         
-        while ll_head.next is not None:
-            if ll_head.next.val not in seen:
-                seen[ll_head.next.val] = True
-                ll_head = ll_head.next
-            else:
-                ll_head.next = ll_head.next.next
-
-        return ll_head_holder
-    
-    # 1- Space = O(1)
-    # 2- time = O(n^2)
-    def remove_dups_without_buffer(self, ll_head):
-        if ll_head is None:
-            return ll_head
         
-        ll_head_holder = ll_head
-
-        while ll_head.next is not None:
-            head_temp = ll_head
-            node_value = ll_head.val
+#         if  (size - kth_to_last) <= 0:
+#             print("Error: kth to last value is larger than linked list size. ")
+#             raise
             
-            while ll_head.next is not None:
-                if node_value == ll_head.next.val:
-                    ll_head.next = ll_head.next.next
-                else:
-                    ll_head = ll_head.next
-                    
-            if head_temp.next is not None:
-                ll_head = head_temp.next
-                
-        return ll_head_holder
+#         # return head
+#         counter = 1
+#         while counter != (size - kth_to_last):
+#             counter += 1
+#             head = head.next
+#         return head
+
+    # using runner method a bit efficient 
+    def return_ll_kth_to_last(head, kth_to_last):
+        p1 = head
+        p2 = head
+        
+        counter = 0 
+        head_copy = head
+        while counter != kth_to_last:
+            if p2 is None:
+                print("Error: kth to last value is larger than linked list size. ")
+                raise
+            
+            p2 = p2.next
+            counter += 1
+        
+        while p2.next is not None:
+            p2 = p2.next
+            p1 = p1.next
+        
+        return p1
     
 def main():
+    temp_list = [1]
+    linked_list_obj = LinkedList()
+    temp_linked_list_head = linked_list_obj.convert_list_to_linked_list(temp_list)
     
-    temp_vals = [1,1,2,3,5,8,1,2,3,1,10,100000,-1,-2,-1]
-    temp_ll = Node()
-    temp_ll_head = temp_ll
-    for val in temp_vals:
-        temp_node =  Node()
-        temp_node.val = val
-        
-        temp_ll.next = temp_node
-        temp_ll = temp_ll.next
+    # linked_list_obj.print_linked_list(temp_linked_list_head)
     
-    temp_ll_head = temp_ll_head.next
-    print("Before")
-    Node().print_linked_list(temp_ll_head)
-    # removed_dups = solution().remove_dups_with_buffer(temp_ll_head)
-    removed_dups = solution().remove_dups_without_buffer(temp_ll_head)
-    print("After")
-    Node().print_linked_list(removed_dups)
+    kth_to_last_node = Solution.return_ll_kth_to_last(temp_linked_list_head, 1)
+    print("kth_to_last_node is = ", kth_to_last_node.val)
 
 if __name__ == "__main__":
     main()
-    
